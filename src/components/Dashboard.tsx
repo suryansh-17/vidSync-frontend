e52d6d9cd4be4afb2c9e92c1bbfb85639143ab4a
suryansh-17 <suripandey17@gmail.com>
2024-08-12 15:01:51 +0530

    build fixes

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div className="flex flex-1">
+      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-700 bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+        <div className="flex gap-2">
+          {[...new Array(4)].map((i) => (
+            <div
+              key={"first-array" + i}
+              className="h-20 w-full rounded-lg  bg-neutral-800 animate-pulse"
+            >
+              hiii
+            </div>
+          ))}
+        </div>
+        <div className="flex gap-2 flex-1">
+          {[...new Array(2)].map((i) => (
+            <div
+              key={"second-array" + i}
+              className="h-full w-full rounded-lg  bg-neutral-800 animate-pulse"
+            >
+              byee
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
